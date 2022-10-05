624bf31aed0d83653695bb7c53b4bf3270559936
Kyle Berry <berrytechnics@gmail.com>
2022-10-05 16:18:56 -0500

    skycon color range

--- src/components/Current.vue
+++ src/components/Current.vue
@@ -5,12 +5,12 @@
         <h3>for {{location.city}}, {{location.state_abbr}} ({{location.zipcode}})</h3>
       </div>
       <div class="center-container">
-        <Skycon size="150" color="#ffff00" v-if="wxIcon=='clear-day'" condition="clear-day" />
-        <Skycon size="150" color="#ccccaa" v-if="wxIcon=='clear-night'" condition="clear-night" />
-        <Skycon size="150" color="#aaaaaa" v-if="wxIcon=='fog'" condition="fog" />
-        <Skycon size="150" color="#999999" v-if="wxIcon=='rain'" condition="rain" />
-        <Skycon size="150" color="#ffffff" v-if="wxIcon=='snow'" condition="snow" />
-        <Skycon size="150" color="#cccccc" v-if="wxIcon=='cloudy'" condition="cloudy" />
+        <Skycon size="150" speed="3" color="#ffff00" v-if="wxIcon=='clear-day'" condition="clear-day" />
+        <Skycon size="150" speed="3" color="#ccccaa" v-if="wxIcon=='clear-night'" condition="clear-night" />
+        <Skycon size="150" speed="3" color="#aaaaaa" v-if="wxIcon=='fog'" condition="fog" />
+        <Skycon size="150" speed="3" color="#999999" v-if="wxIcon=='rain'" condition="rain" />
+        <Skycon size="150" speed="3" color="#ffffff" v-if="wxIcon=='snow'" condition="snow" />
+        <Skycon size="150" speed="3" color="#cccccc" v-if="wxIcon=='cloudy'" condition="cloudy" />
         <h1><span class="text-underline">{{weather.temp}}</span>&deg;<sup>F</sup></h1>
       </div>
       <div class="center-container">
